15a5c16472eea6878fe44d8ff6b6f20e0f198275
ayesha <ayeshakhattana@gmail.com>
2024-06-10 22:49:50 +0400

    Changed th app icon and added and polished th ui for home screen

--- app/src/main/java/com/loc/newsapp/ui/theme/Theme.kt
+++ app/src/main/java/com/loc/newsapp/ui/theme/Theme.kt
@@ -18,14 +18,14 @@ import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     background = Black,
-    primary = Blue,
+    primary = Purple,
     error = DarkRed,
     surface = LightBlack
 )
 
 private val LightColorScheme = lightColorScheme(
     background = Color.White,
-    primary = Blue,
+    primary = Purple,
     error = LightRed,
     surface = Color.White
 )
@@ -33,7 +33,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun NewsAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
